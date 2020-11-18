c73db3fd16c1678928abae6b0eab1e6c08c432db
unknown <csiamunyanga@hotmail.com>
2020-11-18 16:19:34 +0200

    edited the welcome.blade.php file

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {useEffect} from 'react';
 
 function Example() {
+
+    useEffect( ()=> alert('here') );
+
+    let alert = ()=> {
+        alert('here');
+    }
+
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
                         <div className="card-header">Example Component</div>
-
                         <div className="card-body">I'm an example component!</div>
                     </div>
                 </div>
@@ -19,6 +26,6 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById('sky-angel')) {
+    ReactDOM.render(<Example />, document.getElementById('sky-angel'));
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,16 @@
 <!doctype html>
 <html>
 <head>
-    <title>Example Domain</title>
+    <title>Sky Angel</title>
 
     <meta charset="utf-8" />
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <script src='js/app.js'></script>
     <script type="text/javascript" src="http://me.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=tDW9k1cnzllvO6bhjNUATndwEL9w9hIY0i8obAcNZid-sfIM9biNk1hfFlLMzoX9" charset="UTF-8"></script>
-    <link rel="stylesheet" crossorigin="anonymous" href="http://me.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cDovL2V4YW1wbGUuY29tLw"/><style type="text/css">
-    
-    body {
-        background-color: #f0f0f2;
-        margin: 0;
-        padding: 0
-        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-        
-    }
-    div {
-        width: 600px;
-        margin: 5em auto;
-        padding: 2em;
-        background-color: #fdfdff;
-        border-radius: 0.5em;
-        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
-    }
-    a:link, a:visited {
-        color: #38488f;
-        text-decoration: none;
-    }
-    @media (max-width: 700px) {
-        div {
-            margin: 0 auto;
-            width: auto;
-        }
-    }
-    </style>    
+    <link rel="stylesheet" crossorigin="anonymous" href="http://me.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cDovL2V4YW1wbGUuY29tLw"/>
 </head>
-
 <body>
-<div>
-    <h1>Example Domain</h1>
-    <p>This domain is for use in illustrative examples in documents. You may use this
-    domain in literature without prior coordination or asking for permission.</p>
-    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
-</div>
+    <div id='sky-angel'></div>
 </body>
 </html>
