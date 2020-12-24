45768a84f0748418ef44d2fd2758486fc71dddc0
cacious siamunyanga <csiamunyanga@hotmail.com>
2020-12-24 09:35:32 -0800

    processed jsx file to make sure the toHttps module is found

--- public/js/app.js
+++ public/js/app.js
@@ -56258,7 +56258,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _StartGame__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./StartGame */ "./resources/js/components/StartGame.jsx");
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_4__);
-!(function webpackMissingModule() { var e = new Error("Cannot find module 'toHttps'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var _helpers_toHttps__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../helpers/toHttps */ "./resources/js/helpers/toHttps.js");
 function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
@@ -57112,7 +57112,7 @@ var Main = function Main() {
 
 
   var getPlayers = function getPlayers() {
-    url = !(function webpackMissingModule() { var e = new Error("Cannot find module 'toHttps'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(getPlayersUrl, true);
+    url = Object(_helpers_toHttps__WEBPACK_IMPORTED_MODULE_5__["default"])(getPlayersUrl, true);
     console.log('protocol', location.protocol, 'url', url);
     jquery__WEBPACK_IMPORTED_MODULE_4___default.a.ajax({
       url: url,
@@ -57220,6 +57220,52 @@ var StartGame = function StartGame(props) {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (StartGame);
+
+/***/ }),
+
+/***/ "./resources/js/helpers/toHttps.js":
+/*!*****************************************!*\
+  !*** ./resources/js/helpers/toHttps.js ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/**
+ * Changes the protocol of a valid url to/from https
+ * @param {String} url The url to check for protocol
+ * @param {Boolean} bool The choice whether to change url protocol to page protocol
+ * @return {Boolean} as false if it failed
+ * @return {String} returns a string if it was successfull
+ */
+var toHttps = function toHttps(url, bool) {
+  var finalUrl = '';
+
+  if (url.constructor !== String || !Boolean(/^(http)/i.test(url)) || bool.constructor !== Boolean) {
+    return false;
+  }
+
+  var httpsReg = /^(https)/i;
+  var httpReg = /^(http)/i;
+
+  switch (bool) {
+    case true:
+      finalUrl = url.replace(httpReg, 'https');
+      break;
+
+    case false:
+      finalUrl = httpsReg.test(httpsReg, 'http');
+      break;
+
+    default:
+      finalUrl = fasle;
+  }
+
+  return finalUrl;
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (toHttps);
 
 /***/ }),
 
