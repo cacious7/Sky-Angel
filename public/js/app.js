dd8bd718a3bf483774a63c345a1f1b351971500d
unknown <csiamunyanga@hotmail.com>
2021-03-04 15:58:16 +0200

    edit controls and buttons
    create a uniform theme for all buttons to improve ux

--- public/js/app.js
+++ public/js/app.js
@@ -57334,8 +57334,8 @@ var toHttps = function toHttps(url, bool) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\xampp\htdocs\Sky-Angel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\xampp\htdocs\Sky-Angel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\cacious\Documents\Cacious\Sky-Angel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\cacious\Documents\Cacious\Sky-Angel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
